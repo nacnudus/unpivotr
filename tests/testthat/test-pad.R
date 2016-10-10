ae091a5b708c998d1c238d0dfc12524a1102de37
nacnudus <nacnudus@gmail.com>
2016-10-10 22:20:08 +0100

    Remove unnecessary print from test

--- tests/testthat/test-pad.R
+++ tests/testthat/test-pad.R
@@ -5,7 +5,7 @@ test_that("pad() returns predictable cells", {
     tidytable(purpose$`NNW WNW`, FALSE, FALSE) %>% 
     dplyr::filter(!is.na(character)) # Introduce 'holes' in the data
   # Select a region with gaps
-  (bag <- dplyr::filter(cells, row %in% 3:4, col %in% 1:2))
+  bag <- dplyr::filter(cells, row %in% 3:4, col %in% 1:2)
   # Pad the gaps
   expect_error(pad(bag, -1), "'rows' and 'cols' must be >= 0")
   expect_equal(pad(bag), pad(bag, 0L, 0L))
