a7574ae25ea16f6e7337554a407dc28a947b0ad3
nacnudus <nacnudus@gmail.com>
2016-10-17 01:02:36 +0100

    Add tests for 100% line coverage

--- tests/testthat/test-join_header.R
+++ tests/testthat/test-join_header.R
@@ -1,5 +1,6 @@
 context("join_header()")
 
+library(tidyxl)
 library(dplyr)
 
 expect_purpose <- 
@@ -62,6 +63,55 @@ c("Bachelor's degree", "Bachelor's degree", "Bachelor's degree",
 "Postgraduate qualification", "Postgraduate qualification", "Postgraduate qualification", 
 "Postgraduate qualification", "Postgraduate qualification", "Postgraduate qualification", 
 "Postgraduate qualification")
+
+expect_purpose_short <- 
+c("0 - 6", "0 - 6", "0 - 6", "7 - 10", "7 - 10", "7 - 10", "0 - 6", 
+"0 - 6", "0 - 6", "7 - 10", "7 - 10", "7 - 10", "0 - 6", "0 - 6", 
+"7 - 10", "7 - 10", "7 - 10", "0 - 6", "0 - 6", "0 - 6", "7 - 10", 
+"7 - 10", "7 - 10", "0 - 6", "0 - 6", "7 - 10", "7 - 10", "7 - 10", 
+"0 - 6", "0 - 6", "7 - 10", "7 - 10", "7 - 10", "0 - 6", "0 - 6", 
+"0 - 6", "7 - 10", "7 - 10", "7 - 10", "0 - 6", "0 - 6", "0 - 6", 
+"7 - 10", "7 - 10", "7 - 10", "0 - 6", "0 - 6", "0 - 6", "7 - 10", 
+"7 - 10", "7 - 10", "0 - 6", "0 - 6", "7 - 10", "7 - 10")
+
+expect_sex_short <- 
+c("Female", "Female", "Female", "Female", "Female", "Female", 
+"Female", "Female", "Female", "Female", "Female", "Female", "Female", 
+"Female", "Female", "Female", "Female", "Female", "Female", "Female", 
+"Female", "Female", "Female", "Female", "Female", "Female", "Female", 
+"Female", "Male", "Male", "Male", "Male", "Male", "Male", "Male", 
+"Male", "Male", "Male", "Male", "Male", "Male", "Male", "Male", 
+"Male", "Male", "Male", "Male", "Male", "Male", "Male", "Male", 
+"Male", "Male", "Male", "Male")
+
+expect_age_short <- 
+c("15 - 24", "25 - 44", "45 - 64", "15 - 24", "25 - 44", "45 - 64", 
+"15 - 24", "25 - 44", "45 - 64", "15 - 24", "25 - 44", "45 - 64", 
+"25 - 44", "45 - 64", "15 - 24", "25 - 44", "45 - 64", "15 - 24", 
+"25 - 44", "45 - 64", "15 - 24", "25 - 44", "45 - 64", "25 - 44", 
+"45 - 64", "15 - 24", "25 - 44", "45 - 64", "25 - 44", "45 - 64", 
+"15 - 24", "25 - 44", "45 - 64", "15 - 24", "25 - 44", "45 - 64", 
+"15 - 24", "25 - 44", "45 - 64", "15 - 24", "25 - 44", "45 - 64", 
+"15 - 24", "25 - 44", "45 - 64", "15 - 24", "25 - 44", "45 - 64", 
+"15 - 24", "25 - 44", "45 - 64", "25 - 44", "45 - 64", "25 - 44", 
+"45 - 64")
+
+expect_education_short <- 
+c("Bachelor's degree", "Bachelor's degree", "Bachelor's degree", 
+"Bachelor's degree", "Bachelor's degree", "Bachelor's degree", 
+"Certificate", "Certificate", "Certificate", "Certificate", "Certificate", 
+"Certificate", "Diploma", "Diploma", "Diploma", "Diploma", "Diploma", 
+"No Qualification", "No Qualification", "No Qualification", "No Qualification", 
+"No Qualification", "No Qualification", "Postgraduate qualification", 
+"Postgraduate qualification", "Postgraduate qualification", "Postgraduate qualification", 
+"Postgraduate qualification", "Bachelor's degree", "Bachelor's degree", 
+"Bachelor's degree", "Bachelor's degree", "Bachelor's degree", 
+"Certificate", "Certificate", "Certificate", "Certificate", "Certificate", 
+"Certificate", "Diploma", "Diploma", "Diploma", "Diploma", "Diploma", 
+"Diploma", "No Qualification", "No Qualification", "No Qualification", 
+"No Qualification", "No Qualification", "No Qualification", "Postgraduate qualification", 
+"Postgraduate qualification", "Postgraduate qualification", "Postgraduate qualification"
+)
 
 test_that("Compass directions N, NNW, W, and WNW work", {
   cells <- tidytable(purpose$`NNW WNW`, colnames = FALSE)
@@ -161,6 +211,136 @@ test_that("Compass directions SSW and ENE work", {
     arrange(row, col)
   expect_equal(datacells[[4]], expect_education)
   expect_equal(datacells[[5]], expect_sex)
+})
+
+test_that("Compass directions ABOVE and LEFT work", {
+  cells <- tidytable(purpose$`ABOVE LEFT`, colnames = FALSE)
+  row_headers <- 
+    cells %>%
+    filter(col <= 2, !is.na(character)) %>%
+    select(row, col, header = character) %>%
+    split(.$col)
+  col_headers <- 
+    cells %>%
+    filter(row <= 2, !is.na(character)) %>%
+    select(row, col, header = character) %>%
+    split(.$row)
+  datacells <- 
+    cells %>%
+    filter(row >= 3, col >= 3, !is.na(character)) %>%
+    select(row, col, value = as.integer(character)) %>%
+    ABOVE(col_headers[[1]]) %>%
+    LEFT(row_headers[[1]]) %>%
+    arrange(row, col)
+  expect_equal(datacells[[4]], expect_sex_short)
+  expect_equal(datacells[[5]], expect_purpose_short)
+})
+
+test_that("Compass directions BELOW and RIGHT work", {
+  cells <- tidytable(purpose$`BELOW RIGHT`, colnames = FALSE)
+  row_headers <- 
+    cells %>%
+    filter(col >= 7, !is.na(character)) %>%
+    select(row, col, header = character) %>%
+    split(.$col)
+  col_headers <- 
+    cells %>%
+    filter(row >= 11, !is.na(character)) %>%
+    select(row, col, header = character) %>%
+    split(.$row)
+  datacells <- 
+    cells %>%
+    filter(row <= 10, col <= 6, !is.na(character)) %>%
+    select(row, col, value = as.integer(character)) %>%
+    BELOW(col_headers[[2]]) %>%
+    RIGHT(row_headers[[2]]) %>%
+    arrange(row, col)
+  expect_equal(datacells[[4]], expect_sex_short)
+  expect_equal(datacells[[5]], expect_purpose_short)
+})
+
+test_that("Compass directions ABOVE and LEFT work with boundaries", {
+  spreadsheet <- system.file("extdata/purpose.xlsx", package = "unpivotr")
+  cells <- contents(spreadsheet, "ABOVE LEFT border")[[1]]
+  formatting <- formats(spreadsheet)
+  left_borders <- which(!is.na(formatting$local$border$left$style))
+  top_borders <- which(!is.na(formatting$local$border$top$style))
+  left_border_cells <- 
+    cells %>%
+    filter(row == 2, local_format_id %in% left_borders) %>%
+    select(row, col)
+  top_border_cells <- 
+    cells %>%
+    filter(col == 2, local_format_id %in% top_borders) %>%
+    select(row, col)
+  row_headers <- 
+    cells %>%
+    filter(col <= 3, !is.na(character)) %>%
+    select(row, col, header = character) %>%
+    split(.$col)
+  col_headers <- 
+    cells %>%
+    filter(row <= 3, !is.na(character)) %>%
+    select(row, col, header = character) %>%
+    split(.$row)
+  datacells <- 
+    cells %>%
+    filter(row >= 4, col >= 4, !is.na(content)) %>%
+    mutate(content = ifelse(is.na(character), content, NA)) %>%
+    select(row, col, value = as.integer(content)) %>%
+    ABOVE(col_headers[[1]], left_border_cells) %>% # Different from ABOVE LEFT
+    N(col_headers[[2]]) %>% # Same as ABOVE LEFT
+    LEFT(row_headers[[1]], top_border_cells) %>% # Different from ABOVE LEFT
+    W(row_headers[[2]]) %>% # Same as ABOVE LEFT
+    arrange(row, col)
+  expect_equal(datacells[[4]], expect_purpose_short)
+  expect_equal(datacells[[5]], expect_age_short)
+  expect_equal(datacells[[6]], expect_sex_short)
+  expect_equal(datacells[[7]], expect_education_short)
+  expect_error(ABOVE(datacells, col_headers[[1]], left_border_cells[-2, ]))
+  expect_error(LEFT(datacells, row_headers[[1]], top_border_cells[-2, ]))
+})
+
+test_that("Compass directions BELOW and RIGHT work with boundaries", {
+  spreadsheet <- system.file("extdata/purpose.xlsx", package = "unpivotr")
+  cells <- contents(spreadsheet, "BELOW RIGHT border")[[1]]
+  formatting <- formats(spreadsheet)
+  left_borders <- which(!is.na(formatting$local$border$left$style))
+  top_borders <- which(!is.na(formatting$local$border$top$style))
+  left_border_cells <- 
+    cells %>%
+    filter(row == 14, local_format_id %in% left_borders) %>%
+    select(row, col)
+  top_border_cells <- 
+    cells %>%
+    filter(col == 11, local_format_id %in% top_borders) %>%
+    select(row, col)
+  row_headers <- 
+    cells %>%
+    filter(col >= 10, !is.na(content)) %>%
+    select(row, col, header = character) %>%
+    split(.$col)
+  col_headers <- 
+    cells %>%
+    filter(row >= 14, !is.na(content)) %>%
+    select(row, col, header = character) %>%
+    split(.$row)
+  datacells <- 
+    cells %>%
+    filter(row <= 13, col <= 9, !is.na(content)) %>%
+    mutate(content = ifelse(is.na(character), content, NA)) %>%
+    select(row, col, value = as.integer(content)) %>%
+    BELOW(col_headers[[2]], left_border_cells) %>% # Different from BELOW RIGHT
+    S(col_headers[[1]]) %>% # Same as BELOW RIGHT
+    RIGHT(row_headers[[2]], top_border_cells) %>% # Different from BELOW RIGHT
+    E(row_headers[[1]]) %>% # Same as BELOW RIGHT
+    arrange(row, col)
+  expect_equal(datacells[[4]], expect_purpose_short)
+  expect_equal(datacells[[5]], expect_age_short)
+  expect_equal(datacells[[6]], expect_sex_short)
+  expect_equal(datacells[[7]], expect_education_short)
+  expect_error(BELOW(datacells, col_headers[[1]], left_border_cells[-2, ]))
+  expect_error(RIGHT(datacells, row_headers[[1]], top_border_cells[-2, ]))
 })
 
 test_that("join_header() works", {
