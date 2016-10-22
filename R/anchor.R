d30a8d1d18d2ec679211c1aa4b6345fe2b04e016
nacnudus <nacnudus@gmail.com>
2016-10-22 16:45:04 +0100

    Support multi-cell anchor()s

--- R/anchor.R
+++ R/anchor.R
@@ -1,26 +1,36 @@
 #' Filter for a cell from which to select others
 #'
-#' @description \code{anchor} protects you from selecting an initial cell that
-#' does not exist.  This is useful when choosing an inital cell from which to
-#' search for others, using \code{\link{offset}} and \code{\link{extend}}.
-#' If the chosen cell does not exist, then cell is generated at the given
-#' row and column, with NA in every column other than 'row' and 'col'.
+#' @description \code{anchor} protects you from selecting initial cells that do
+#' not exist.  This is useful when choosing inital cells from which to search
+#' for others, using \code{\link{offset}} and \code{\link{extend}}.  If the
+#' chosen cells do not exist, then cells are generated at the given rows and
+#' columns, with NAs in every column other than 'row' and 'col'.
 #' @param cells Data frame, the cells among which the anchor will be sought
-#' @param row Numeric, the row of the intended cell
-#' @param col Numeric, the column of the intended cell
+#' @param rows Numeric, the rows of the intended cells
+#' @param cols Numeric, the columns of the intended cell
+#' @param cross Logical, whether to 'cross' the given rows and columns to
+#' generate all combinations. Unless this is TRUE, \code{rows} and \code{cols}
+#' must be the same length.  TRUE by default.
 #' @export
 #' @examples
 #' cells <- tidytable(purpose$`NNW WNW`)
 #' \dontrun{
 #'   anchor(cells, 0, 1)
+#'   anchor(cells, 1, 1:2)
 #' }
 #' anchor(cells, 1, 1)
+#' anchor(cells, 1, 1:2, cross = TRUE)
 #' anchor(cells, 100, 1)
-anchor <- function(cells, row, col) {
-  if (row <= 0 || col <= 0) {stop("'row' and 'col' must both be >= 1")}
-  out <- cells[cells$row == row & cells$col == col, ]
-  if (nrow(out) == 0) {
-    out <- dplyr::bind_rows(out, tibble::tibble(row = row, col = col))
-  }
-  out
+anchor <- function (cells, rows, cols, cross = TRUE) {
+    if (any(c(rows, cols) <= 0)) {
+        stop("Elements of 'rows' and 'cols' must all be >= 1")
+    }
+    rows <- as.integer(rows)
+    cols <- as.integer(cols)
+    if (cross) {
+      left <- tidyr::crossing(row = rows, col = cols)
+    } else {
+      left <- tibble::data_frame(row = rows, col = cols)
+    }
+    dplyr::left_join(left, cells, by = c("row", "col"))
 }
--- tests/testthat/test-anchor.R
+++ tests/testthat/test-anchor.R
@@ -2,7 +2,8 @@ context("anchor()")
 
 test_that("a valid cell is returned", {
   cells <- tidytable(purpose$`NNW WNW`)
-  expect_error(anchor(cells, 1, 0), "'row' and 'col' must both be >= 1")
+  expect_error(anchor(cells, 1, 0),
+               "Elements of 'rows' and 'cols' must all be >= 1")
   expect_equal(anchor(cells, 1, 1),
                cells[cells$row == 1 & cells$col == 1, ])
   # Check that an invented row is blank besides valid row/col
