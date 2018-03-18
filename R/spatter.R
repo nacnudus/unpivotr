cc99ad80884e6be723bdbeff0de0bd3919ff3455
Duncan Garmonsway <nacnudus@gmail.com>
2018-03-18 16:20:52 +0000

    Remove commented code

--- R/spatter.R
+++ R/spatter.R
@@ -79,20 +79,3 @@ spatter.data.frame <- function(.data, key, values = NULL, types = data_type) {
     # 2nd pass because factors are doubly listed
     dplyr::mutate_at(factors, concatenate)
 }
-
-# spatter.data.frame <- function(cells, key, types = data_type) {
-#   key <- rlang::ensym(key)
-#   types <- rlang::ensym(types)
-#   type_names <- unique(dplyr::pull(cells, !! types))
-#   factors <-
-#     cells %>%
-#     dplyr::distinct(!! key, !! types) %>%
-#     dplyr::filter(!! types %in% c("fct", "ord")) %>%
-#     dplyr::pull(!! key)
-#   cells %>%
-#     pack(types = !! types, name = ".value") %>%
-#     tidyr::spread(!! key, .value) %>%
-#     dplyr::mutate_if(is.list, concatenate) %>%
-#     # 2nd pass because factors are doubly listed
-#     dplyr::mutate_at(factors, concatenate)
-# }
