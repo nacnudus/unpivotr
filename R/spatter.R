06704d6b53a70fbd1c006b1e2c3de83b10c296a8
Duncan Garmonsway <nacnudus@gmail.com>
2018-03-26 00:15:15 +0100

    Tweak spatter() docs

--- R/spatter.R
+++ R/spatter.R
@@ -1,6 +1,9 @@
 #' Spatter a key-values set across multiple columns.
 #'
 #' @param data A data frame.
+#' @param values Optional. The column of `cells` to use as the values of each
+#' cell.  Given as a bare variable name.  If omitted (the default), the `types`
+#' argument will be used instead.
 #' @export
 #' @examples
 #' path <- system.file("extdata", "worked-examples.xlsx", package = "unpivotr")
@@ -20,45 +23,41 @@
 #'   dplyr::select(row, data_type, header, character, numeric) %>%
 #'   spatter(header)
 #'
-#' # The difference between spatter() and tidyr::spread() is that spatter() has
-#' # to know which data-type to use for each cell beneath the headers.  By
-#' # default, it looks at the data_type column to decide, but you can choose a
-#' # different column.
+#' # The difference between spatter() and tidyr::spread() is that spatter()
+#' # needs to know which data-type to use for each cell beneath the headers.  By
+#' # default, it looks at the `data_type` column to decide, but you can change
+#' # that with the `types` argument.
 #' cells %>%
 #'   behead(N, header) %>%
 #'   dplyr::mutate(data_type_2 = "character") %>%
 #'   dplyr::select(row, data_type_2, header, character) %>%
 #'   spatter(header, types = data_type_2)
 #'
-#' # You can instead name one specific column to use for the cell values. In
-#' # this case we use the `address` column, which also help to show what
+#' # Alternatively you can name one specific column to use for the cell values.
+#' # In this case we use the `address` column, which also helps to show what
 #' # spatter() does.
 #' cells %>%
 #'   behead(N, header) %>%
 #'   dplyr::select(row, header, address) %>%
 #'   spatter(header, values = address)
 #'
-#' cells %>%
-#'   behead(N, header) %>%
-#'   dplyr::select(row, col, header) %>%
-#'   spatter(header, values = col)
-#'
-#' # The column used for the values is consumed before the spread occurs, so if
-#' # it's necessary for demarking the rows, you'll need to create a copy of it
-#' # first.  Otherwise you'll get an error like
-#' # "Duplicate identifiers for rows ..."
+#' # The column used for the values is consumed before the spread occurs. If
+#' # it's necessary for demarking the rows, then make a copy of it first,
+#' # otherwise you'll get an error like "Duplicate identifiers for rows ..."
 #' cells %>%
 #'   behead(N, header) %>%
 #'   dplyr::mutate(row2 = row) %>%
 #'   dplyr::select(row, header, row2) %>%
 #'   spatter(header, values = row2)
 #'
-#' # spatter() does the minimum necessary coercion of data types so that values
-#' # can coexist in the same column.  You can control the conversion yourself by
-#' # supplying custom functions, named according to the `data_type` column. If
-#' # your custom functions aren't sufficient to avoid the need for coercion,
-#' # then they will be overridden.  Ordered factors in particular will always be
-#' # coerced to unordered factors.
+#' # spatter() automatically converts data types so that they can coexist in the
+#' # same column.  Ordered factors in particular will always be coerced to
+#' # unordered factors.
+#'
+#' # You can control the conversion yourself by supplying custom functions,
+#' # named by the data type of the cells they are to convert (look at the
+#' # `data_type` column).  If your custom functions aren't sufficient to avoid
+#' # the need for coercion, then they will be overridden.
 #' cells %>%
 #'   behead(N, header) %>%
 #'   dplyr::select(row, data_type, header, character, numeric) %>%
