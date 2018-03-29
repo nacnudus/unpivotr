6004c9af02aae85b1cc1586d1734540b27421531
Duncan Garmonsway <nacnudus@gmail.com>
2018-03-29 23:49:20 +0100

    Avoid locale-dependent behaviour in spatter() test

--- tests/testthat/test-spatter.R
+++ tests/testthat/test-spatter.R
@@ -1,5 +1,8 @@
 context("spatter")
 
+# upper/lowercase sort order depends on the locale
+colnames(BOD) <- c("time", "demand")
+
 x <- tidy_table(BOD, FALSE, TRUE)
 y <-
   tidy_table(BOD, FALSE, TRUE) %>%
@@ -13,10 +16,10 @@ z <-
 
 test_that("spatter() works", {
   d <- spatter(y, header)
-  expect_equal(colnames(d), c("row", "demand", "Time"))
+  expect_equal(colnames(d), c("row", "demand", "time"))
   expect_equal(nrow(d), 6L)
   expect_equal(purrr::map_chr(d, class),
-               c(row = "integer", demand = "numeric", Time = "numeric"))
+               c(row = "integer", demand = "numeric", time = "numeric"))
 })
 
 test_that("spatter() can use row and col as headers", {
@@ -46,42 +49,42 @@ test_that("spatter() can use data_type as both types and header", {
 
 test_that("spatter() doesn't delete data_type if it isn't used", {
   d <- spatter(z, header, types = data_type_2)
-  expect_equal(colnames(d), c("row", "data_type", "demand", "Time"))
+  expect_equal(colnames(d), c("row", "data_type", "demand", "time"))
   expect_equal(nrow(d), 6L)
   expect_equal(purrr::map_chr(d, class),
                c(row = "integer",
                  data_type = "character",
                  demand = "character",
-                 Time = "character"))
+                 time = "character"))
 })
 
 test_that("spatter() does delete the 'values' column", {
   d <- spatter(z, header, values = data_type_2)
-  expect_equal(colnames(d), c("row", "chr", "demand", "Time"))
+  expect_equal(colnames(d), c("row", "chr", "demand", "time"))
   expect_equal(nrow(d), 6L)
   expect_equal(purrr::map_chr(d, class),
                c(row = "integer",
                  chr = "character",
                  demand = "character",
-                 Time = "character"))
+                 time = "character"))
 })
 
 test_that("spatter() can use data_type as both types and values", {
   d <- spatter(dplyr::select(y, -dbl), header, values = data_type)
-  expect_equal(colnames(d), c("row", "demand", "Time"))
+  expect_equal(colnames(d), c("row", "demand", "time"))
   expect_equal(nrow(d), 6L)
   expect_equal(purrr::map_chr(d, class),
                c(row = "integer",
                  demand = "character",
-                 Time = "character"))
+                 time = "character"))
 })
 
 test_that("spatter() can use header as both header and values", {
   d <- spatter(dplyr::select(y, -dbl, -data_type), header, values = header)
-  expect_equal(colnames(d), c("row", "demand", "Time"))
+  expect_equal(colnames(d), c("row", "demand", "time"))
   expect_equal(nrow(d), 6L)
   expect_equal(d$demand, rep("demand", 6L))
-  expect_equal(d$Time, rep("Time", 6L))
+  expect_equal(d$time, rep("time", 6L))
 })
 
 test_that("spatter() retains factors", {
@@ -157,7 +160,7 @@ test_that("spatter() can use custom functions for converting data types", {
   expect_equal(purrr::map_chr(d, class),
                `names<-`(c("integer", "character", rep("integer", 6L)),
                          c("col", 1:7)))
-  expect_equal(d$`1`, c("Time-header", "demand-header"))
+  expect_equal(d$`1`, c("time-header", "demand-header"))
 })
 
 test_that("spatter() works on common data types", {
