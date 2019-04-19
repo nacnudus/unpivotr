aaa02845fecc8e1b5e4d0c791559e575e549280d
Ian Moran <ian.moran11@gmail.com>
2019-04-19 13:15:53 +1000

    fixed nnw bug

--- R/migrate.R
+++ R/migrate.R
@@ -10,7 +10,7 @@
 #' @export
 
 migrate <- function(orientated_df){
-
+  
   orientated_df_nested <-
     orientated_df %>%
     filter(!is.na(.orientation)) %>% 
@@ -18,29 +18,36 @@ migrate <- function(orientated_df){
     mutate(value = coalesce(character,as.character(numeric))) %>%
     select(row,col,value,.orientation,.header_group) %>%
     nest()
-
+  
   data_row_index <- which(orientated_df_nested$.orientation == "data")
   header_dfs   <- orientated_df_nested$data[orientated_df_nested$.orientation != "data"]
   orientations <- orientated_df_nested$.orientation[orientated_df_nested$.orientation != "data"]
   header_names <- orientated_df_nested$.header_group[orientated_df_nested$.orientation != "data"]
-
+  
+  header_dfs <- 
+    map2(header_dfs,header_names, function(header_df, header_name){
+      header_df %>% 
+        rename(!!sym(header_name) := value)
+    })
+  
   list(
     x = header_dfs,
-    y = orientations,
-    z = header_names) %>%
-    pmap(function(x,y,z){
-      enhead_tabledata(header_data = x, direction = y,header_label = z,
+    y = orientations) %>%
+    pmap(function(x,y){
+      enhead_tabledata(header_data = x, direction = y,
                        values = orientated_df_nested$data[[data_row_index]])} ) %>%
-    reduce(full_join,by = c("row", "col", "value.data")) %>%
-    rename(value = value.data)
+    reduce(full_join,by = c("row", "col","value")) 
+  
 
+  
 }
+
 
 #' A wrapper to enhead.
 #'
 #' @description
 #' This function is used by [unpivotr::migrate] to create a tidy data frame using a `pmap` style workflow.
-#'
+#'d
 #' @param header_data a list of data frames that are header subsets from the original data frame produced by tidyxl.
 #' @param direction a vector of orientations
 #' @param header_label a vector of names for variables associated with each header group.
@@ -49,12 +56,11 @@ migrate <- function(orientated_df){
 #' @name enhead_tabledata
 
 
-enhead_tabledata <- function(header_data, direction,
-                             header_label, values = tabledata) {
+enhead_tabledata <- function(header_data, direction, values = tabledata) {
   unpivotr::enhead(
     data_cells = values,
     header_cells = header_data,
-    direction = direction) %>%
-    rename(!!sym(header_label) := value.header)
+    direction = direction) 
 }
+
 
