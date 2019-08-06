82a778f0af7b8ba8b596ec717101fdb42cbc85ae
Ian Moran <ian.moran11@gmail.com>
2019-08-06 21:16:38 +1000

    Added all direction to plot_cells

--- R/plot_directions.R
+++ R/plot_directions.R
@@ -16,42 +16,54 @@ plot_cells <- function(sheet, text = values, interactive = FALSE) {
     mutate(.direction = "\U2610", .header_label = "data")
   
   
+    
   if (interactive == FALSE) {
     sheet_01 <-
       bind_rows(sheet, data_cells) %>%
       mutate(.arrow = case_when(
-        .direction == "W" ~ "\U2192",
-        .direction == "N" ~ "\U2193",
-        .direction == "NNW" ~ "\U21B3",
+        .direction == "N"   ~ "\U2193",
+        .direction == "NNE" ~ "\U21B1", 
+        .direction == "ENE" ~ "\U21B3",
+        .direction == "E"   ~ "\U2190",
+        .direction == "ESE" ~ "\U21B2",
+        .direction == "SSE" ~ "\U21B0",     
+        .direction == "S"   ~ "\U2191",
+        .direction == "SSW" ~ "\U21B1",
+        .direction == "WSW" ~ "\U21B0",
+        .direction == "W"   ~ "\U2192",
         .direction == "WNW" ~ "\U21B1",
-        T ~ NA_character_
-      )) %>% 
+        .direction == "NNW" ~ "\U21B3")) %>% 
       mutate(values = coalesce(as.character(numeric),as.character(character),
                                as.character(logical),as.character(date)))
     
     bind_rows(
       mutate(sheet_01, .arrow = NA, set = "Cell values"),
-      mutate(sheet_01, {{text}} := NA, set = "Directions")
-    ) %>%
+      mutate(sheet_01, {{text}} := NA, set = "Directions")) %>%
       ggplot(aes(x = col, y = -row, fill = .header_label)) + geom_tile() +
       geom_text(aes(label = str_sub({{text}}, 1, 5))) +
-      geom_text(aes(label = ifelse(!.direction %in% c("WNW"), .arrow, NA))) +
-      geom_text(aes(label = ifelse(.direction %in% c("WNW"), .arrow, NA)), angle = -90) +
+      geom_text(aes(label = ifelse(!.direction %in% c("NNE","ESE","WSW","ENE"), .arrow, NA))) +
+      geom_text(aes(label = ifelse(.direction %in% c("NNE","ESE","WSW"), .arrow, NA)), angle = 270) +
+      geom_text(aes(label = ifelse(.direction %in% c("ENE"), .arrow, NA)), angle = 270) +
       facet_wrap(~set, scales = "free")
   } else {
     
     sheet_01 <-
       bind_rows(sheet, data_cells) %>%
       mutate(.arrow = case_when(
-        .direction == "W" ~ "\U2192",
-        .direction == "N" ~ "\U2193",
-        .direction == "NNW" ~ "\U21B3",
-        .direction == "WNW" ~ "\U21B4",
-        T ~ NA_character_
-      )) %>%  
+        .direction == "N"   ~ "\U2193",
+        .direction == "NNE" ~ "\U21B1", 
+        .direction == "ENE" ~ "\U21B1",
+        .direction == "E"   ~ "\U2190",
+        .direction == "ESE" ~ "\U21B2",
+        .direction == "SSE" ~ "\U21B0",     
+        .direction == "S"   ~ "\U2191",
+        .direction == "SSW" ~ "\U21B1",
+        .direction == "WSW" ~ "\U21B0",
+        .direction == "W"   ~ "\U2192",
+        .direction == "WNW" ~ "\U21B1",
+        .direction == "NNW" ~ "\U21B3")) %>%  
       mutate(values = coalesce(as.character(numeric),as.character(character),
                                as.character(logical),as.character(date)))
-    
     
     plot_object <-
       bind_rows(
