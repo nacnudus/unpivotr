dc652a10ba456e3dfbac27bc6ffe4029cef9f8a7
Ian Moran <ian.moran11@gmail.com>
2019-08-10 22:07:32 +1000

    Add dplyr wrappers.

---
+++ R/dplyr_verbs.R
@@ -0,0 +1,67 @@
+#' Add a format variable
+#' @description 
+#' Add a format variable.
+#' @param cells a data frame created by tidyxl::xlsx_cells 
+#' @param fmt_function fmt_* function. 
+#' @export
+
+append_fmt <- function(cells, fmt_function){
+  
+  formats <- attr(cells,"formats")
+  data_cells <- attr(cells,"data_cells")
+  
+  cells <- 
+    cells %>% 
+    mutate({{fmt_function}} := exec({{fmt_function}},
+                                    format_id_vec = cells$local_format_id, 
+                                    sheet_formats = formats))
+  attr(cells,"formats") <- formats
+  attr(cells,"data_cells") <- data_cells
+  
+  cells
+  
+}
+
+#' Select keeping formats 
+#' @description 
+#' A wrapper for dplyr::select that retains formatting information 
+#' @param df a data frame created by tidyxl::xlsx_cells 
+#' @param dots select input. 
+#' @export
+
+select_fmt <- function(df, ...){
+  
+  data_cells <- attr(df,"data_cells")
+  formats    <- attr(df,"formats")
+  
+  select_quos <-   enquos(...) 
+  
+  df <- select(df,!!!select_quos)
+  
+  attr(df,"data_cells") <- data_cells
+  attr(df,"formats") <- formats
+  
+  df  
+}
+
+#' Filter keeping formats 
+#' @description 
+#' A wrapper for dplyr::filter that retains formatting information 
+#' @param df a data frame created by tidyxl::xlsx_cells 
+#' @param dots filter input. 
+#' @export
+
+filter_fmt <- function(df, ...){
+  
+  data_cells <- attr(df,"data_cells")
+  formats    <- attr(df,"formats")
+  
+  filter_quos <-   enquos(...) 
+  
+  df <- filter(df,!!!filter_quos)
+  
+  attr(df,"data_cells") <- data_cells
+  attr(df,"formats") <- formats
+  
+  df  
+}
