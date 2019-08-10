d23f9ec44d2ca45437089d0d273037fe5f0be807
Ian Moran <ian.moran11@gmail.com>
2019-08-10 22:00:58 +1000

    Add grouping function. Second try

--- R/grouings.R
+++ R/grouings.R
@@ -1,7 +1,7 @@
 #' Groupings 
 #' @description 
 #' This functions passes grouping expressions to the .groupings argument of locate_groups. It works most naturually with fmt_* functions.
-#' @dots gouring expressions 
+#' @param  dots gouring expressions 
 #'
 #' @export
 
